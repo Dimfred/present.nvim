8744eafb7c708ab36ba21c0cc37ff14e4b197814
Dmitrij Vinokour <dmitrij.vinokour@non.se.com>
2024-08-13 10:37:54 +0200

    feat: remove nvim_has_feature

--- plugin/present.lua
+++ plugin/present.lua
@@ -1,18 +1,9 @@
-if not vim.api.nvim_has_feature 'nvim-0.5' then
-  vim.api.nvim_echo(
-    { { 'Present needs Neovim >= 0.5', 'WarningMsg' } },
-    true,
-    {}
-  )
-  return
-end
-
-vim.api.nvim_create_user_command('Present', function()
-  require('present').present()
-end, { desc = 'Toggle Presentation view' })
-vim.api.nvim_create_user_command('PresentEnable', function()
-  require('present').present(true)
-end, { desc = 'Enable Presentation view' })
-vim.api.nvim_create_user_command('PresentDisable', function()
-  require('present').present(false)
-end, { desc = 'Disable Presentation view' })
+vim.api.nvim_create_user_command("Present", function()
+    require("present").present()
+end, { desc = "Toggle Presentation view" })
+vim.api.nvim_create_user_command("PresentEnable", function()
+    require("present").present(true)
+end, { desc = "Enable Presentation view" })
+vim.api.nvim_create_user_command("PresentDisable", function()
+    require("present").present(false)
+end, { desc = "Disable Presentation view" })
